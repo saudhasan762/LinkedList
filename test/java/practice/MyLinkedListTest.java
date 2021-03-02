9857daa9ebe5dd73d30ba44ea3e80453a14a714d
saudhasan762 <azanali152@gmail.com>
2021-03-02 21:55:18 +0530

    UC7 - Searching a node in the Linked List

--- test/java/practice/MyLinkedListTest.java
+++ test/java/practice/MyLinkedListTest.java
@@ -94,4 +94,18 @@ public class MyLinkedListTest {
                 myLinkedList.tail.equals(mySecondNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3numbersWhenLinkedListShouldAbleToSearchANode() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNode();
+        boolean result = myLinkedList.search(mySecondNode);
+        Assert.assertTrue(result);
+    }
 }
