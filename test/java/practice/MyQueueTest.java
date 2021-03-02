dc669864fcd51ce6e2d760dccf757b53beebfca7
saudhasan762 <azanali152@gmail.com>
2021-03-02 23:48:19 +0530

    UC4 - S&Q Removing an element from beginning of a queue

--- test/java/practice/MyQueueTest.java
+++ test/java/practice/MyQueueTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class MyQueueTest {
     @Test
-    public void given3numbersInQueueFirstPushedNodeShouldBeAtTop(){
+    public void given3numbersInQueueFirstEnqueueNodeShouldBeAtTop(){
         MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(56);
@@ -17,5 +17,21 @@ public class MyQueueTest {
         myQueue.printQueue();
         INode peak = myQueue.peak();
         Assert.assertEquals(myThirdNode,peak);
+    }
+
+    @Test
+    public void given3numbersInQueueDequeueNodeShouldMatchWithFirstAddedNode(){
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyQueue myQueue = new MyQueue();
+
+        myQueue.enqueue(myThirdNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myFirstNode);
+        myQueue.printQueue();
+        INode dequeueValue = myQueue.dequeue();
+        myQueue.printQueue();
+        Assert.assertEquals(myThirdNode,dequeueValue);
     }
 }
