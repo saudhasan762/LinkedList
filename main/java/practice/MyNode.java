f7b0c865e6ee810ed40684ab29a159616ee06b65
saudhasan762 <azanali152@gmail.com>
2021-03-02 21:15:22 +0530

    UC1 - created a simple Linked List

---
+++ main/java/practice/MyNode.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class MyNode<K> {
+    private K key;
+    private MyNode next;
+
+    public MyNode(K key){
+        this.key=key;
+        this.next=null;
+    }
+    public MyNode getNext(){
+        return next;
+    }
+    public void setNext (MyNode next) {
+        this.next = next;
+    }
+}
