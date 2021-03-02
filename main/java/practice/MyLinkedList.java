f6c82c13b90b59b119cefc30f03d7d131b589d5a
saudhasan762 <azanali152@gmail.com>
2021-03-02 21:45:41 +0530

    UC5 - Deleting the first Element

--- main/java/practice/MyLinkedList.java
+++ main/java/practice/MyLinkedList.java
@@ -51,4 +51,10 @@ public class MyLinkedList {
         myNode.setNext(newNode);
         newNode.setNext(tempNode);
     }
+
+    public INode pop() {
+        INode tempnode = this.head;
+        this.head = head.getNext();
+        return tempnode;
+    }
 }
--- test/java/practice/MyLinkedListTest.java
+++ test/java/practice/MyLinkedListTest.java
@@ -56,4 +56,23 @@ public class MyLinkedListTest {
                 myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3numbersWhenLinkedListShouldAbleToDeleteFirst() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.pop();
+        System.out.println("Deleting first Element: ");
+        myLinkedList.printMyNode();
+
+        boolean result = myLinkedList.head.equals(mySecondNode) &&
+                myLinkedList.head.getNext().equals(myThirdNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
 }
