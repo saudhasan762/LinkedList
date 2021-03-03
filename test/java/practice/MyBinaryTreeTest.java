482be1de4061f6c3166c993d8a151dfdb4c60bcd
saudhasan762 <azanali152@gmail.com>
2021-03-03 20:00:33 +0530

    UC2 BT - Adding more Elements into a Binary Tree

--- test/java/practice/MyBinaryTreeTest.java
+++ test/java/practice/MyBinaryTreeTest.java
@@ -6,12 +6,32 @@ import org.junit.Test;
 public class MyBinaryTreeTest {
 
     @Test
-    public void given3Numbers_WhnAddedToBinaryTree_ShouldReturnSize3(){
+    public void given3Numbers_WhenAddedToBinaryTree_ShouldReturnSize3(){
         MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
         myBinaryTree.add(56);
         myBinaryTree.add(30);
         myBinaryTree.add(70);
         int size = myBinaryTree.getSize();
         Assert.assertEquals(3,size);
+    }
+
+    @Test
+    public void givenNumbers_WhenAddedToBinaryTree_ShouldReturnSize3(){
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(13,size);
     }
 }
